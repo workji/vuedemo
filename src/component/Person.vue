e50ca333811b73e219b91b881c4d448fdb0cd0fa
workji910 <workji910@gmail.com>
2025-12-21 11:07:41 +0900

    change css

--- src/component/Person.vue
+++ src/component/Person.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="container person">
+    <div class="container border border-primary mt-4 p-4">
         <h2>姓名：{{name}}</h2>
         <h2>年龄：{{age}}</h2>
-        <button @click="changeName">修改名字</button>
-        <button @click="changeAge">年龄+1</button>
-        <button @click="showTel">点我查看联系方式</button>
+        <button class="btn btn-outline-primary me-2" @click="changeName">修改名字</button>
+        <button class="btn btn-outline-primary me-2" @click="changeAge">年龄+1</button>
+        <button class="btn btn-outline-primary" @click="showTel">点我查看联系方式</button>
     </div>
 </template>
 
