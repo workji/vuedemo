9604cf1709377727985b9cb55d788afd66da37a4
workji910 <workji910@gmail.com>
2025-12-22 19:51:32 +0900

    watch ref

--- src/component/Person.vue
+++ src/component/Person.vue
@@ -1,37 +1,46 @@
 <template>
   <div class="mt-4 mb-4">
-    <h1>情况一：监视【ref】定义的【基本类型】数据</h1>
-    当前求和SUM={{ sum }}<br>
-    <button class="btn btn-outline-primary me-2" @click="addOne">点我加1</button>
-    <button class="btn btn-outline-primary me-2" @click="subOne">点我减1</button>
+    <h1>情况二：监视【ref】定义的【对象类型】数据</h1>
+    <h2>姓名: {{ person.name }}</h2>
+    <h2>年龄: {{ person.age }}</h2>
+    <button class="btn btn-outline-primary me-2" @click="changeName">更改名字</button>
+    <button class="btn btn-outline-primary me-2" @click="changeAge">更改年龄</button>
+    <button class="btn btn-outline-primary me-2" @click="changePerson">更改整个人</button>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { ref, watch } from 'vue';
+import { ref, watch } from "vue";
+let person = ref({
+  name: "张三",
+  age: 18,
+});
 
-const sum = ref(0);
-
-function addOne() {
-  sum.value += 1;
+function changeName() {
+  person.value.name += "!";
 }
-function subOne() {
-  sum.value -= 1;
+
+function changeAge() {
+  person.value.age += 1;
 }
 
-// 监视【ref】定义的【基本类型】数据
-// watch监听sum的变化 watch(监听的变量, 变量变化后执行的回调函数){}
-// 这里注意sum是一个ref类型，但是在watch中监听时，不需要加sum.value
-const stopWatch = watch(sum, (newVal, oldVal)=>{
-  console.log('sum的值变化了，现在是：' + sum.value + '，之前是：' + oldVal);
+function changePerson() {
+  person.value = {
+    name: "李四",
+    age: 20,
+  };
+}
 
-  if (sum.value >= 10) {
-    console.log('sum的值大于等于10了，停止监听');
-    // 停止监听
-    stopWatch();
-  }
-})
-
+// 监视【ref】定义的【对象类型】数据，监视的是对象的地址值
+// 若想监视对象内部属性的变化，需要手动开启深度监视
+//    watch的第一个参数是：被监视的数据
+//    watch的第二个参数是：监视的回调
+//    watch的第三个参数是：配置对象（deep、immediate等等.....） 
+watch(person, (newVal, oldVal) => {
+  console.log("person对象发生了变化");
+  console.log("旧值:", oldVal);
+  console.log("新值:", newVal);
+}, { deep: true });
 </script>
 
 <style scoped></style>
