89e9c6d0ade0bba302b61a20cd5ca41400d102b8
workji910 <workji910@gmail.com>
2025-12-22 20:09:53 +0900

    reactive

--- src/component/Person.vue
+++ src/component/Person.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="mt-4 mb-4">
-    <h1>情况二：监视【ref】定义的【对象类型】数据</h1>
+    <h1>情况三：监视【reactive】定义的【对象类型】数据</h1>
     <h2>姓名: {{ person.name }}</h2>
     <h2>年龄: {{ person.age }}</h2>
     <button class="btn btn-outline-primary me-2" @click="changeName">更改名字</button>
@@ -10,37 +10,40 @@
 </template>
 
 <script lang="ts" setup>
-import { ref, watch } from "vue";
-let person = ref({
+import { reactive, watch } from "vue";
+let person = reactive({
   name: "张三",
   age: 18,
 });
 
 function changeName() {
-  person.value.name += "!";
+  person.name += "!";
 }
 
 function changeAge() {
-  person.value.age += 1;
+  person.age += 1;
 }
 
 function changePerson() {
-  person.value = {
+  // reactive定义的对象不可以整体替换，否则会失去响应式 特性
+  // person.value = {
+  //   name: "李四",
+  //   age: 20,
+  // };
+
+  Object.assign(person, {
     name: "李四",
     age: 20,
-  };
+  });
 }
 
-// 监视【ref】定义的【对象类型】数据，监视的是对象的地址值
-// 若想监视对象内部属性的变化，需要手动开启深度监视
-//    watch的第一个参数是：被监视的数据
-//    watch的第二个参数是：监视的回调
-//    watch的第三个参数是：配置对象（deep、immediate等等.....） 
+// 监视【reactive】定义的【对象类型】数据
+// 且默认是开启深度监视的, 无需额外配置
 watch(person, (newVal, oldVal) => {
   console.log("person对象发生了变化");
   console.log("旧值:", oldVal);
   console.log("新值:", newVal);
-}, { deep: true });
+});
 </script>
 
 <style scoped></style>
