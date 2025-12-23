8d9d426fb6591fa9d5344f9874f00f0bb6cd798e
workji910 <workji910@gmail.com>
2025-12-23 18:47:18 +0900

    ref property

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
-  <Person />
+  <Person ref="ren" />
+  <button class="btn btn-outline-primary me-2" @click="showLog">点击我</button>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
+  import {ref} from 'vue'
   import Person from "@/component/Person.vue";
-  export default {
-    name: "App",
-    components: {
-      Person,
-    },
-  };
+
+  let ren = ref()
+
+  // 用在组件标签上，获取的是组件实例对象。
+  function showLog() {
+    console.log(ren.value.name) // defineExpose暴露了，可以获取到
+    // console.log(ren.value.age) // 没有暴露，所以获取不到
+  }
 </script>
 
 <style scoped>
-
 </style>
--- src/component/Person.vue
+++ src/component/Person.vue
@@ -1,44 +1,27 @@
 <template>
-  <div class="mt-4 mb-4">
-    <h1>需求：水温达到50℃，或水位达到20cm，则联系服务器</h1>
-    <h2 id="demo">水温：{{temp}}</h2>
-    <h2>水位：{{height}}</h2>
-    <button class="btn btn-outline-primary me-2" @click="changePrice">水温+1</button>
-    <button class="btn btn-outline-primary me-2" @click="changeSum">水位+10</button>
+  <div class="m-4 border">
+    <h1 ref="title1">尚硅谷</h1>
+    <h2 ref="title2">前端</h2>
+    <h3 ref="title3">Vue</h3>
+    <input type="text" ref="inpt"> <br><br>
+    <button class="btn btn-outline-primary me-2" @click="showLog">点我打印内容</button>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { ref, watch, watchEffect } from 'vue';
-let temp = ref(20);
-let height = ref(0);
+  import { ref, defineExpose } from 'vue';
+  let title2 = ref()
 
-function changePrice() {
-  temp.value += 10;
-}
-
-function changeSum() {
-  height.value += 10;
-}
-
-// 用watch实现，需要明确的指出要监视：temp、height
-// 这里的问题是，如果监视多个响应式数据，需要用数组写很多监视，比较麻烦
-// 
-watch([temp, height], ([newTemp, newHeight]) => {
-  // 室温达到50℃，或水位达到20cm，立刻联系服务器
-  if (newTemp >= 50 || newHeight >= 20) {
-    console.log('联系服务器');
+  // 用在普通DOM HTML标签上，获取的是`DOM`节点。
+  function showLog() {
+    console.log(title2.value.innerHTML);
   }
-});
 
-// 用watchEffect实现，不需要明确指出要监视哪些数据, 会自动根据实现来监视
-// 而且会立即执行一次好像watch的immediate选项
-watchEffect(() => {
-  // 室温达到50℃，或水位达到20cm，立刻联系服务器
-  if (temp.value >= 50 || height.value >= 20) {
-    console.log('联系服务器--watchEffect');
-  }
-});
+  // 将组件中的数据交给外部使用
+  let name = ref('张三')
+  let age = ref(18)
+  // 只有允许的才会expose暴露给外部使用
+  defineExpose({name})
 </script>
 
 <style scoped></style>
