66f707990d1116447060b2e82dd36fb350c3bcf1
workji910 <workji910@gmail.com>
2025-12-23 19:39:59 +0900

    props

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <Person />
+  <Person name="aaa" age="10" />
+  <Person name="bbb" age="16" />
+  <Person name="ccc" age="20" />
 </template>
 
 <script lang="ts" setup>
--- src/component/Person.vue
+++ src/component/Person.vue
@@ -1,45 +1,23 @@
 <template>
   <div class="m-4 border">
-    <h1>姓名:{{person.name}}</h1>
-    <h2>年龄:{{person.age}}</h2>
-    <hr />
-    <div v-for="(item, index) in persons" :key="index" class="m-2 p-2 border">
-      <h3>姓名:{{item.name}} - 年龄:{{item.age}}</h3>
-      <button @click="persons.splice(index, 1)">删除</button>
-    </div>
+    <span>{{name}} - {{age}}</span>
+    <button @click="showLog">Click Me</button>
   </div>
 </template>
 
 <script lang="ts" setup>
-  import { ref, reactive } from 'vue';
-  import { type PersonType, type Persons } from '@/types/index';
+import { defineProps } from "vue";
+const x = defineProps(['name', 'age']);
 
-  let person:PersonType = reactive({
-    name: "张三",
-    age: 18,
-  })
-
-  let persons:Persons = reactive([
-    { name: "李四", age: 20 },
-    { name: "王五", age: 22 },
-    { name: "赵六", age: 24 },
-  ]);
-
-  let personList:Array<PersonType> = reactive([
-    { name: "李四", age: 20 },
-    { name: "王五", age: 22 },
-    { name: "赵六", age: 24 },
-  ]);
-
-  let personsRef = ref<Persons>([
-    { name: "小红", age: 14 },
-    { name: "小刚", age: 15 },
-  ]);
-  let personRef = ref<PersonType>({ name: "小明", age: 16 });
-  let personListRef = ref<Array<PersonType>>([
-    { name: "小红", age: 14 },
-    { name: "小刚", age: 15 },
-  ]);
+function showLog()  {
+  console.log(x.name, x.age);
+}
 </script>
 
-<style scoped></style>
+<style scoped>
+  .border {
+    border: 2px solid;
+    border-color: #42b983;
+    margin: 4px;
+  }
+</style>
