8fec4816fda34b56e4c2cf38c0f0743da186d553
workji910 <workji910@gmail.com>
2025-12-23 20:17:59 +0900

    good

--- src/component/Person.vue
+++ src/component/Person.vue
@@ -15,8 +15,8 @@ import { type Persons } from "@/types";
 // 单纯接受属性，如果父组件传递错误类型，这里不会有任何警告或报错
 // defineProps(["name", "age", "list"]);
 
-// 使用类型定义属性，接受 + 限制类型
-withDefaults(defineProps<{name:string, age:number, list:Persons}>(), {
+// 第三种写法：接收 + 限制类型 + 指定默认值 + 限制必要性?
+withDefaults(defineProps<{name:string, age:number, list?:Persons}>(), {
   name: "默认名字",
   age: 0,
   list: () => [
